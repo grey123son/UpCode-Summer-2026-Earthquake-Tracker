009e2a90915c628a9b5c283f2c9292ad2fecb836
AjK-PS <166272711+lmSleepyy@users.noreply.github.com>
2026-06-18 14:03:04 -0700

    Wrote findTheta method

--- unrelatedGeometryProblem.py
+++ unrelatedGeometryProblem.py
@@ -5,4 +5,15 @@ def intersect(x1, y1, x2, y2, b1, b2): # calcs intersect location
     A = np.array([[x1, y1], [x2, y2]])
     b = np.array([b1, b2])
     return np.linalg.solve(A, b)
-    # output is [x, y] of intersect
+    # output is [x, y] of intersect 
+
+def findTheta(x1, y1, x2, y2): # input is x and y of CHOSEN POINT, then x and y of other point
+    if (x2 - x1) == 0:
+        return np.pi / 2
+    elif y2 - y1 == 0:
+        if (x2 - x1) > 0:
+            return 0
+        else:
+            return np.pi
+    else:
+        return np.arctan2(y2 - y1, x2 - x1)
