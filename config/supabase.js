78ae51264193bcf916c6a8796d7e2b9faf9e3e95
Wearily <56124939+Wearily@users.noreply.github.com>
2026-06-18 10:33:09 -0700

    finished connection.js

---
+++ config/supabase.js
@@ -0,0 +1,3 @@
+export const SUPA_API_URL = "https://lejveivwlyrotymexzem.supabase.co/rest/v1/earthquakes";
+
+export const SUPA_API_KEY = "sb_publishable_Q94aJl0egnEtmAivKlEPCQ_-YRdp8hQ";
--- connection.js
+++ connection.js
@@ -1,1 +1,20 @@
-async function
+async function getEarthquakes({
+    minTime,
+    maxTime,
+    minMagnitude,
+    maxMagnitude,
+    isEarthquake.
+    limit = 20,
+}) {
+    const params = new URLSearchParams({
+        select: "id,mag,time,type,title,x,y,z",
+        order: "time.desc",
+        limit,
+    });
+
+    if (minTime) params.append("time", `gte.${minTime}`);
+    if (maxTime) params.append("time", `gte.${maxTime}`);
+
+    if (minMagnitude !=)
+
+}
