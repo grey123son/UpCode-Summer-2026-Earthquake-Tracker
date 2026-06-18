9c59158c214b42e1a80f32a7bdf9a5d959fea779
AjK-PS <166272711+lmSleepyy@users.noreply.github.com>
2026-06-18 15:11:55 -0700

    okay its work now
    yup

--- unrelatedGeometryProblem.py
+++ unrelatedGeometryProblem.py
@@ -1,4 +1,42 @@
-import math
+# PI = 3.141592653589793
+# HALF_PI = 1.5707963267948966
+    
+# def taylorArctan(y, x):
+#     # Returns the angle in radians between -pi and pi
+#     if x == 0:
+#         if y > 0: return HALF_PI
+#         if y < 0: return -HALF_PI
+#         return 0.0 # Undefined, but defaults to 0
+
+#     # Use the Taylor series for arctan
+#     z = y / x
+#     result = z
+#     term = z
+#     n = 1
+
+#     while abs(term) > 1e-10:
+#         term *= -z * z
+#         result += term / (2 * n + 1)
+#         n += 1
+
+#     # Quadrant adjustment logic
+#     if x > 0:
+#         return result
+#     elif x < 0 and y >= 0:
+#         return result + PI
+#     else:
+#         return result - PI
+
+# def findTheta(x1, y1, x2, y2): # input is x and y of CHOSEN POINT, then x and y of other point
+#     if (x2 - x1) == 0:
+#         return PI / 2
+#     elif y2 - y1 == 0:
+#         if (x2 - x1) > 0:
+#             return 0
+#         else:
+#             return PI
+#     else:
+#         return taylorArctan(y2 - y1, x2 - x1)
 
 def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
     # Calculate coordinate differences
@@ -21,18 +59,7 @@ def intersect(x1, y1, x2, y2, x3, y3, x4, y4):
     x = det(d, xdiff) / div
     y = det(d, ydiff) / div
     
-    return x, y
-
-def findTheta(x1, y1, x2, y2): # input is x and y of CHOSEN POINT, then x and y of other point
-    if (x2 - x1) == 0:
-        return math.pi / 2
-    elif y2 - y1 == 0:
-        if (x2 - x1) > 0:
-            return 0
-        else:
-            return math.pi
-    else:
-        return math.atan2(y2 - y1, x2 - x1)
+    return [float(x), float(y)]
 
 useLater = [0, 1, 2, 3]
 
@@ -40,25 +67,48 @@ def diagonal_crossing(points):
     # points is a list of [x, y] coordinates
     # returns the intersection point of the diagonals
     # screw codingbat for using python2 btw
-    minYi = 0
-    minY = points[0][1]
-    for i in range(1, len(points)):
-        if points[i][1] < minY:
-            minY = points[i][1]
-            minYi = i
 
-    useLater.remove(minYi)
+
+    # minXi = 0
+    # minX = points[0][0]
+    # for i in range(1, len(points)):
+    #     if points[i][0] < minX:
+    #         minX = points[i][0]
+    #         minXi = i
+
+    # useLater.remove(minXi)
     
-    thetas = [-1, -1, -1, -1] 
-    # we chose min y so we're guaranteed to be in quadrant 1 or 2, meaning REAL thetas between 0 and pi
-    for i in range(len(points)):
-        if i != minYi:
-            thetas[i] = findTheta(points[minYi][0], points[minYi][1], points[i][0], points[i][1])
+    # thetas = [-10, -10, -10, -10] 
+    # # we chose min x so we're guaranteed to be in quadrant 1 or 4, meaning REAL thetas between -pi and pi
+    # for i in range(len(points)):
+    #     if i != minXi:
+    #         thetas[i] = findTheta(points[minXi][0], points[minXi][1], points[i][0], points[i][1])
 
-    maxTheta = thetas.index(max(thetas))
-    useLater.remove(maxTheta)
+    # print(thetas)
 
-    if thetas[useLater[0]] > thetas[useLater[1]]:
-        return intersect(points[minYi][0], points[minYi][1], points[useLater[0]][0], points[useLater[0]][1], points[maxTheta][0], points[maxTheta][1], points[useLater[1]][0], points[useLater[1]][1])
-    else:
-        return intersect(points[minYi][0], points[minYi][1], points[useLater[1]][0], points[useLater[1]][1], points[maxTheta][0], points[maxTheta][1], points[useLater[0]][0], points[useLater[0]][1])
+    # maxTheta = thetas.index(max(thetas))
+    # useLater.remove(maxTheta)
+
+    # if thetas[useLater[0]] > thetas[useLater[1]]:
+    #     return intersect(points[minXi][0], points[minXi][1], points[useLater[0]][0], points[useLater[0]][1], points[maxTheta][0], points[maxTheta][1], points[useLater[1]][0], points[useLater[1]][1])
+    # else:
+    #     return intersect(points[minXi][0], points[minXi][1], points[useLater[1]][0], points[useLater[1]][1], points[maxTheta][0], points[maxTheta][1], points[useLater[0]][0], points[useLater[0]][1])
+
+    minX = min(points, key=lambda p: p[0])[0]
+    minY = min(points, key=lambda p: p[1])[1]
+    maxX = max(points, key=lambda p: p[0])[0]
+    maxY = max(points, key=lambda p: p[1])[1]
+
+    inter = intersect(points[0][0], points[0][1], points[1][0], points[1][1], points[2][0], points[2][1], points[3][0], points[3][1])
+    if inter is not None and inter[0] > minX and inter[1] > minY and inter[0] < maxX and inter[1] < maxY:
+        return inter
+    
+    inter = intersect(points[0][0], points[0][1], points[2][0], points[2][1], points[1][0], points[1][1], points[3][0], points[3][1])
+    if inter is not None and inter[0] > minX and inter[1] > minY and inter[0] < maxX and inter[1] < maxY:
+        return inter
+    
+    return intersect(points[0][0], points[0][1], points[3][0], points[3][1], points[1][0], points[1][1], points[2][0], points[2][1])
+
+print(diagonal_crossing(
+[[0, 1], [0, 5], [4, 1], [4, 5]]
+    ))
